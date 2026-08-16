57229dd859a476b9e7a364b7205db517293b1989
Qotiph Albyayadh <114820578+qotiph10@users.noreply.github.com>
2026-08-16 13:04:11 +0300

    Add: asteroid-collision - submission-2

---
+++ Data Structures & Algorithms/asteroid-collision/submission-2.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        if(asteroids.length <= 1){
+            return asteroids;
+        }
+        Stack<Integer> st = new Stack<>();
+        st.push(asteroids[0]);
+        for(int i =1 ; i < asteroids.length ;i++){
+            int num = asteroids[i];
+            if(!st.isEmpty()){
+                while(!st.isEmpty()){
+                    if(st.peek() > 0 && num < 0){
+                        if(Math.abs(st.peek()) > Math.abs(num)){
+                            System.out.println(num+" -> Distoried");
+                            num = 0;
+                            break;
+                        }
+                        else if(Math.abs(st.peek()) < Math.abs(num)){
+                            System.out.println(st.peek()+" -> Distroied");
+                            st.pop();
+                        }
+                        else{
+                            System.out.println(st.peek()+" and "+num+" -> Distroied");
+                            st.pop();
+                            num = 0;
+                            break;
+                        }
+                    }
+                    else{
+                        System.out.println("pushing! "+num);
+                        st.push(num);
+                        num = 0;
+                        break;  
+                    }
+                }
+                if(num != 0){
+                    st.push(num);
+                }
+            }
+            else{
+                st.push(num);
+            }
+             
+        }
+        int size = st.size();
+        int ans[] = new int[st.size()];
+        int index = 0;
+        while (!st.isEmpty()) {
+            ans[size - index - 1] = st.pop();
+            index++;
+        }
+        return ans;
+    }
+}
