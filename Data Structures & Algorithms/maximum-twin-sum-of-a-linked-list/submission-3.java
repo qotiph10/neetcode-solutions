57c60d503dc0c967a42b45e4ea40955e8065f7ec
Qotiph Albyayadh <114820578+qotiph10@users.noreply.github.com>
2026-08-19 08:20:08 +0300

    Add: maximum-twin-sum-of-a-linked-list - submission-3

---
+++ Data Structures & Algorithms/maximum-twin-sum-of-a-linked-list/submission-3.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        int index = 0 , size = -1 , ans = 0;
+        ListNode temp = head;
+        do{
+            ListNode temp2 = temp;
+            int sum = 0;
+            if(size == -1){
+                int c = 1;
+                while(temp2.next != null){
+                    c+=1;
+                    temp2 = temp2.next;
+                }
+                size = c;
+                //System.out.println("found size:"+size);
+            }
+            else{
+                int i = index;
+                while(i != (size - index -1)){
+                    temp2 = temp2.next;
+                    i+=1;
+                }
+                //System.out.println(index+"<-- twin of -->"+ i );
+            }
+            sum = temp.val + temp2.val;
+            index+=1;
+            temp = temp.next;
+            ans = Math.max(ans , sum);
+        }while(index < size/2);
+        return ans;
+    }
+}
